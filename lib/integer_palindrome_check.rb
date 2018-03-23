0a027ffcd7324f80daaf407b3b5ef17231b77e2e
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-03-22 18:17:13 -0700

    Number into array, all tests passing

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,37 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+
+  return false if number.nil? || number < 0
+
+  digits = []
+
+  until number == 0
+
+    digits << (number % 10)
+
+    number /= 10
+
+  end
+
+  front = 0
+  back = digits.length - 1
+
+  until front >= back
+
+    while digits[front] == " "
+      front += 1
+    end
+
+    while digits[back] == " "
+      back -= 1
+    end
+
+    return false if digits[front] != digits[back]
+
+    front += 1
+    back -= 1
+  end
+
+  return true
+
 end
